171ba84df8ad97a0741d50e1375853af288c50ac
Glem <glem920813@gmail.com>
2024-08-24 12:56:53 +0800

    update port

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.delete('/students/:id', async (req, res) => {
     }
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 })
