0c00fdf75309b0042cc15c52f334761667069f83
Glem <glem920813@gmail.com>
2024-08-25 16:22:26 +0800

    change database in mysql

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 module.exports = {
     development: {
-        client: 'sqlite3',
+        client: 'mysql2',
         connection: {
-            filename: './database/students.db3' 
+            host: process.env.DB_HOST || 'localhost',
+            user: process.env.DB_USER || 'root',
+            password: process.env.DB_PASSWORD || '0813',
+            database: process.env.DB_DATABASE || 'students'
         },
-        useNullAsDefault: true
     }
 };
